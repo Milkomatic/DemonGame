7239bb5405d05fe20dfc1f5b7cc4f4e6034ee6f7
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-01-26 02:31:26 -0500

    Added distinct placeholder Sprites for the Items to test out the Inventory system

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -41,12 +41,30 @@ public class Inventory : MonoBehaviour {
 
 
     public void TryPickupItem() {
-        int index = _items.Count;
+        if (_nearbyItems.Count == 0)
+            return;
+
+        // Place item in the first available slot
+        // If no slots are available then just return
         Item item = _nearbyItems[0];
-        _items.Add(item);
+        int index = -1;
+        bool pickedUp = false;
+        for (index = 0; index < _items.Count; ++index) {
+            if (_items[index] == null) {
+                _items[index] = item;
+                pickedUp = true;
+                break;
+            }
+        }
+        if (!pickedUp)
+            return;
+
+        // Do pickup actions
         _nearbyItems.RemoveAt(_nearbyItems.Count - 1);  // Faster than removing the zero index
 
-        ItemUISlots[index].sprite = item.InventorySprite;
+        Image img = ItemUISlots[index];
+        img.sprite = item.InventorySprite;
+        img.enabled = true;
 
         item.gameObject.SetActive(false);
     }
@@ -59,7 +77,7 @@ public class Inventory : MonoBehaviour {
         if (item == null)
             return;
 
-        _items.RemoveAt(index);
+        _items[index] = null;
 
         Image img = ItemUISlots[index];
         img.sprite = null;
