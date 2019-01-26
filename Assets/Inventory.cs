4cb1f996b2cbfbc127ca2dc4dc54a3dd5b63a23a
Dan Vicarel <Rabadash8820>
2019-01-25 22:02:59 -0500

    Added the Inventory script and some map GameObjects

---
+++ Assets/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Inventory : MonoBehaviour {
+
+    public UnityEvent Derp;
+    public void UseItem(int index) { }
+
+
+
+}
