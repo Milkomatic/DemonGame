b6899c12915e49f5a015b663a33bf4a99140b5f0
Scott Campbell <SLCampbell96@gmail.com>
2019-01-26 10:53:11 -0500

    setting up room triggers

---
+++ Assets/RoomTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Camera.main.transform.position = this.gameObject.transform.position;
+    }
+}
