c7917917bad481ddaa8ea69ae3f569b863ab6a7d
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-01-27 02:17:53 -0500

    Cleaned up the start/gameover effects

--- Assets/Demon.cs
+++ Assets/Demon.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Demon : MonoBehaviour {
@@ -6,10 +7,14 @@ public class Demon : MonoBehaviour {
     public float TimeLeft;      // Seconds
     public bool IsBanished;
 
+    public UnityEvent Timeout = new UnityEvent();
+
     private void Update() {
         TimeLeft -= Time.deltaTime;
-        if (TimeLeft < 0 && !IsBanished)
-            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        if (TimeLeft < 0 && !IsBanished) {
+            enabled = false;
+            Timeout.Invoke();
+        }
     }
 
     public void Banish() {
--- Assets/GameStateManager.cs
+++ Assets/GameStateManager.cs
@@ -4,31 +4,66 @@ using UnityEngine.UI;
 
 public class GameStateManager : MonoBehaviour {
 
-    private float _fadeT = -1f;
+    private float _effectT = -1f;
+    private float _effectDur;
+    private bool _flashing = false;
+    private bool _startFading = false;
+    private bool _gameOverFading = false;
 
-    public float FadeTime = 5f;
+    public float FadeDuration = 5f;
+    public float FlashDuration = 0.25f;
     public Image FadeImage;
+    public Image GameOverFlashImage;
 
     private void Update() {
-        Color color = FadeImage.color;
-        if (_fadeT >= FadeTime) {
-            _fadeT = -1f;
-            play();
+        // If the effect is over, start the next one, or load the next Scene
+        if (_effectT >= _effectDur) {
+            if (_startFading)
+                play();
+            else if (_gameOverFading)
+                gameOver();
+            else if (_flashing) {
+                _flashing = false;
+                _gameOverFading = true;
+                _effectT = 0f;
+                _effectDur = FadeDuration;
+                GameOverFlashImage.gameObject.SetActive(false);
+            }
         }
-        if (_fadeT >= 0f) {
-            FadeImage.color = new Color(color.r, color.g, color.b, Mathf.Lerp(0f, 1f, _fadeT / FadeTime));
-            _fadeT += Time.deltaTime;
+
+        // Otherwise, continue the effect
+        if (_effectT >= 0f) {
+            if (!_flashing) {
+                Color color = FadeImage.color;
+                float startAlpha = _gameOverFading ? 0.25f : 0f;
+                FadeImage.color = new Color(color.r, color.g, color.b, Mathf.Lerp(startAlpha, 1f, _effectT / _effectDur));
+            }
+            _effectT += Time.deltaTime;
         }
     }
 
     public void Play(bool immediately) {
-        if (!immediately && FadeImage != null)
-            _fadeT = 0f;
+        if (!immediately && FadeImage != null) {
+            _effectT = 0f;
+            _startFading = true;
+            _effectDur = FadeDuration;
+        }
         else
             play();
+    }
+    public void GameOver(bool immediately) {
+        if (!immediately && FadeImage != null) {
+            _effectT = 0f;
+            _flashing = true;
+            GameOverFlashImage.color = new Color(GameOverFlashImage.color.r, GameOverFlashImage.color.g, GameOverFlashImage.color.b, 1f);
+            _effectDur = FlashDuration;
+        }
+        else
+            gameOver();
     }
     public void Quit() => Application.Quit(0);
 
     private void play() => SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    private void gameOver() => SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
 
 }
