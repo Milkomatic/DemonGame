6bd221d0ca46576ae3f7edd4efd59d540a1afa29
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-01-27 00:00:46 -0500

    Added GameOver and Start menu assets

---
+++ Assets/GameStateManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour {
+
+    public void Play() => SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    public void Quit() => Application.Quit(0);
+
+}
