20f8ec9a07e1b86675df53226fb077b7b8e04b25
Scott Campbell <SLCampbell96@gmail.com>
2019-01-26 21:53:40 -0500

    First Puzzle applying items

---
+++ Assets/PuzzleElementHandler.cs
@@ -0,0 +1,18 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [System.Serializable]
+// public class ItemEntry
+// {
+//     public ItemType type;
+//     public GameObject sprite;
+// }
+
+// public class PuzzleElementHandler : MonoBehaviour
+// {
+//     public ItemEntry[] ItemEntry;     
+//     void itemPlaced(ItemType item){
+//        // if(item = )
+//     }
+// }
