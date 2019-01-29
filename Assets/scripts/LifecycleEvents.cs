8567167a8cb37004abb1dfeb8fc3651f2bf2612f
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-01-28 20:15:36 -0500

    The opening animation is now activated by the loading of the Scene, so it can be disabled in the Editor

---
+++ Assets/scripts/LifecycleEvents.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class LifecycleEvents : MonoBehaviour {
+
+    public UnityEvent Awaking = new UnityEvent();
+    public UnityEvent Starting = new UnityEvent();
+    public UnityEvent Enabled = new UnityEvent();
+    public UnityEvent Disabled = new UnityEvent();
+
+    private void Awake() => Awaking.Invoke();
+    private void Start() => Starting.Invoke();
+    private void OnEnable() => Enabled.Invoke();
+    private void OnDisable() => Disabled.Invoke();
+
+}
